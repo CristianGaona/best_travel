f3f0c181dfe04d35d67a9cb3585df49a15dff358
Cristian Gaona <crgaonas24@gmail.com>
2024-07-15 22:57:37 -0500

    Check the git rebase

--- src/main/java/com/best/travel/best_travel/BestTravelApplication.java
+++ src/main/java/com/best/travel/best_travel/BestTravelApplication.java
@@ -124,7 +124,7 @@ public class BestTravelApplication implements CommandLineRunner {
 
 
     }
- //Esto es un cambio que solo lo tiene develop, pero quiero que también lo tenga feature/add_data_jpa
-
+ //Esto es una modificación que solo lo tiene develop, pero quiero que también lo tenga feature/add_data_jpa
+ // Agregamos lineas de código adicionales
 
 }
