4b525102b34de96e327fcc2dc7827e31d0cecb7e
Cristian Gaona <crgaonas24@gmail.com>
2025-08-02 22:40:43 -0500

    feat: Implement FlyService with pagination and sorting capabilities

---
+++ src/main/java/com/best/travel/best_travel/infraestructure/services/FlyService.java
@@ -0,0 +1,71 @@
+package com.best.travel.best_travel.infraestructure.services;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.best.travel.best_travel.api.models.responses.FlyResponse;
+import com.best.travel.best_travel.domain.entity.FlyEntity;
+import com.best.travel.best_travel.domain.repository.FlyRepository;
+import com.best.travel.best_travel.infraestructure.asbtract_services.IFlyService;
+import com.best.travel.best_travel.util.SortType;
+
+import lombok.AllArgsConstructor;
+
+@Transactional
+@Service
+@AllArgsConstructor
+public class FlyService implements IFlyService {
+
+    private final FlyRepository flyRepository;
+
+    @Override
+    public Page<FlyResponse> readAll(Integer page, Integer size, SortType sortType) {
+        PageRequest pageRequest = null;
+        switch (sortType) {
+            case NONE -> pageRequest = PageRequest.of(page, size);
+            case LOWER -> pageRequest = PageRequest.of(page, size, Sort.by(FIELD_BY_SORT).ascending());
+            case UPPER -> pageRequest = PageRequest.of(page, size, Sort.by(FIELD_BY_SORT).descending());
+        }
+        return this.flyRepository.findAll(pageRequest)
+                .map(this::entityToResponse);
+    }
+
+    @Override
+    public Set<FlyResponse> readLessPrice(BigDecimal price) {
+        return this.flyRepository.selectLessPrice(price)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<FlyResponse> readBetweenPrices(BigDecimal min, BigDecimal max) {
+        return this.flyRepository.selectBetweenPrices(min, max)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<FlyResponse> readByOriginDestiny(String origin, String destiny) {
+        return this.flyRepository.selectOriginDestiny(origin, destiny)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+
+    private FlyResponse entityToResponse(FlyEntity fly) {
+        var response = new FlyResponse();
+        BeanUtils.copyProperties(fly, response);
+        return response;
+    }
+    
+}
