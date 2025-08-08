58702a1173c292025ad250b3bfa5e7ba69a299e5
Cristian Gaona <crgaonas24@gmail.com>
2025-08-08 16:43:26 -0500

    feat: Add report generation functionality with Excel export in ReportController and ExcelService

---
+++ src/main/java/com/best/travel/best_travel/api/models/api/controllers/ReportController.java
@@ -0,0 +1,44 @@
+package com.best.travel.best_travel.api.models.api.controllers;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.best.travel.best_travel.infraestructure.asbtract_services.IReportService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+
+
+@RestController
+@RequestMapping(path = "report")
+@AllArgsConstructor
+@Tag(name = "Report", description = "Report API")
+public class ReportController {
+    
+    private final IReportService reportService;
+
+    @GetMapping
+    public ResponseEntity<Resource> get(){
+        var headers = new HttpHeaders();
+        headers.setContentType(FORCE_DOWNLOAD);
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, FORCE_DOWNLOAD_HEADER_VALUE);
+        var fileInBytes = this.reportService.readFile();
+        ByteArrayResource response = new ByteArrayResource(fileInBytes);
+        return ResponseEntity.ok()
+            .headers(headers)
+            .contentLength(fileInBytes.length)
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(response);
+
+    }
+
+    private static final MediaType FORCE_DOWNLOAD = new MediaType("application", "force-download");
+    private static final String FORCE_DOWNLOAD_HEADER_VALUE ="attachment; filename=report.xlsx";
+
+}
---
+++ src/main/java/com/best/travel/best_travel/infraestructure/asbtract_services/IReportService.java
@@ -0,0 +1,6 @@
+package com.best.travel.best_travel.infraestructure.asbtract_services;
+
+public interface IReportService {
+    
+    public byte[] readFile();
+}
